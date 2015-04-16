3e973214a37a96a42ad051816e5a04b16872894b
Louis-Olivier Brassard <louis.oli.br@gmail.com>
2015-04-16 08:56:28 -0400

    Initial commit, make grid

---
+++ Gruntfile.js
@@ -0,0 +1,97 @@
+module.exports = function(grunt) {
+
+	require('load-grunt-tasks')(grunt);
+
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON("package.json"),
+
+		// Setup paths
+		paths: {
+			src: 'src/',
+			dist: 'dist/'
+		},
+
+		banner:
+		'/** \n' +
+		' * @author <%= pkg.author %> \n' +
+		' * @version <%= pkg.version %> \n' +
+		' * @description <%= pkg.description %> \n' +
+		' * @license <%= pkg.license %> \n' +
+		' */ ',
+
+		version: {
+			project: {
+				src: ['package.json']
+			},
+
+			html: {
+				options: {
+					// @version X.X.X
+					prefix: '@version\\s*'
+				},
+				src: ['index.html']
+			},
+			scss: {
+				options: {
+					// @version X.X.X
+					prefix: '@version\\s*'
+				},
+				src: ['<%= paths.src %>*.scss']
+			}
+		},
+		sass: {
+			// compile different stylesheets to be loaded async
+			compile: {
+				files: {
+					'<%= paths.dist %><%= pkg.name %>-<%= pkg.version %>.css': '<%= paths.src %>main.scss'
+				},
+				options: {
+					style: 'compressed', // expanded for dev, compressed for production
+					sourcemap: 'file'
+				}
+			},
+
+			dist: {
+				files: {
+					'<%= paths.dist %><%= pkg.name %>-<%= pkg.version %>.css': '<%= paths.src %>main.scss'
+				},
+				options: {
+					style: 'compressed', // expanded for dev, compressed for production
+					sourcemap: 'none'
+				}
+			},
+
+			options: {
+				precision: 3
+			}
+		},
+
+		// watch: recompile on file save
+		watch: {
+			sass: {
+				files: ['<%= paths.src %>**/*.scss'],
+				tasks: ['sass:compile']
+			}
+		}
+	});
+
+	grunt.registerTask(
+		'compile',
+		'Compile without minification',
+		['version', 'sass:compile']
+	);
+
+	grunt.registerTask(
+		'dev',
+		'Serve the site and auto-regenerate on file change',
+		['compile', 'watch']
+	);
+
+	grunt.registerTask(
+		'default',
+		'Default task - compile the files',
+		['compile']
+	);
+
+};
